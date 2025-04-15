eb11423c2188aa699ba85fa34c3aee608f57bcf3
will.lai <will.lai@temu.com>
2025-04-15 17:47:10 +0800

    feat: add struct MI which supports module operations for int

--- cp_template.cpp
+++ cp_template.cpp
@@ -29,6 +29,49 @@ void cases(int n) {
 #endif
 }
 
+struct MI {
+  int x = 0, M = 998244353;
+  MI() {}
+  MI(int _x) { x = (_x % M + M) % M; }
+
+  MI &operator+=(MI o) {
+    add(o);
+    return *this;
+  }
+  MI operator+(MI o) { return o += x; }
+  MI &operator-=(MI o) {
+    *this += -o.x;
+    return *this;
+  }
+  MI operator-(MI o) { return MI(x) -= o; }
+  MI &operator*=(MI o) {
+    mul(o);
+    return *this;
+  }
+  MI operator*(MI o) { return o *= x; }
+  MI &operator/=(MI o) {
+    *this *= o.inv();
+    return *this;
+  }
+  MI operator/(MI o) { return MI(x) /= o; }
+  MI inv() { return pow(M - 2); }
+  MI pow(int y) {
+    MI t(1), b(x);
+    for (; y; y >>= 1, b *= b)
+      if (y & 1)
+        t *= b;
+    return t;
+  }
+  friend MI operator+(int lhs, const MI &rhs) { return MI(lhs) + rhs; }
+  friend MI operator-(int lhs, const MI &rhs) { return MI(lhs) - rhs; }
+  friend MI operator*(int lhs, const MI &rhs) { return MI(lhs) * rhs; }
+  friend MI operator/(int lhs, const MI &rhs) { return MI(lhs) / rhs; }
+
+private:
+  void add(MI y) { x = (x % M + y.x % M) % M; }
+  void mul(MI y) { x = 1ll * x * y.x % M; }
+};
+
 void solve() {}
 
 int main() {
